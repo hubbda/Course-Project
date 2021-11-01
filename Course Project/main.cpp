9603349845a395800651c6e1a8f36e5e6ea3a499
Devon Hubbard <devon.hubbd@gmail.com>
2021-11-01 14:05:21 -0400

    Initial Commit

---
+++ Course Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Course Project
+//
+//  Created by Devon Hubbard on 11/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
